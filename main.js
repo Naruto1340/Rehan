e4568be5fb92760a16cf1a07aae2f998109c6d78
Naruto1340 <eyerehan50@gmail.com>
2024-10-29 01:15:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -781,3 +781,14 @@ window.addEventListener('resize', () => {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
+const playButton = document.getElementById('play-button');
+const audio = document.getElementById('audio');
+
+playButton.addEventListener('click', () => {
+    // Check if the audio is already playing
+    if (audio.paused) {
+        audio.play(); // Play the audio
+    } else {
+        audio.pause(); // Pause the audio if it's already playing
+    }
+});
